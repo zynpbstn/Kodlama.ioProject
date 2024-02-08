ae6c8c605bd74bd3c9d60bd15bb0287597b91ee5
zynpb <zynpb@TUNCAYAILESI>
2024-02-08 15:01:41 +0300

    Add project files.

---
+++ Kodlama.ioProject/Entities/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using Kodlama.io_Project.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io_Project.Entities.Concrete
+{
+
+    //Çıplak class kalmasın.
+    public class Category : IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+
+}
---
+++ Kodlama.ioProject/Entities/Concrete/User.cs
@@ -0,0 +1,19 @@
+﻿using Kodlama.io_Project.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io_Project.Entities.Concrete
+{
+    //Çıplak class kalmasın.
+    public class User : IEntity
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
